c30063f05c96b43abf22cd16da7dca0c3ce8b287
Alex Oliveira <iamsralex@gmail.com>
2025-09-18 10:30:19 -0300

    Integrando o autocomplete no sistema

--- core/itbi_scraper.py
+++ core/itbi_scraper.py
@@ -1,17 +1,19 @@
 import requests
 
 class AutoCompleteITBI:
-    def __init__(self, cep:str):
+    def __init__(self, cep: str):
         self.URL = "https://viacep.com.br/ws/"
-        self.cep = cep
-        self.cep = self.cep.split("-")
-        self.cep = "".join(self.cep)
-        if self.cep[0] != "0":
-            self.cep  = "0" + self.cep
-        if len(self.cep) != 8:
-            return None
-        
+        self.cep = cep.replace("-", "").strip()
+        if len(self.cep) == 7:
+            self.cep = "0" + self.cep
+
     def get_data(self):
-        URL = self.URL + self.cep + "/json/"
-        resp = requests.get(URL)
-        return resp.json()
+        if len(self.cep) != 8 or not self.cep.isdigit():
+            return None
+        resp = requests.get(f"{self.URL}{self.cep}/json/")
+        if resp.status_code == 200:
+            data = resp.json()
+            if "erro" in data:
+                return None
+            return data
+        return None
